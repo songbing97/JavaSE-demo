72b7e2d44d056bcbff3b05ee09b83a8933d7f830
songbing97 <lansahaibing@qq.com>
2020-08-01 17:33:14 +0800

    add all

---
+++ src/com.song/SocketClient0.java
@@ -0,0 +1,26 @@
+package com.song;
+
+import java.io.*;
+import java.net.Socket;
+
+public class SocketClient0 {
+    public static void main(String[] args) {
+        String serverName = args[0];
+        int port = Integer.parseInt(args[1]);
+        try{
+            System.out.println("连接到主机：" + serverName + ",端口号：" + port );
+            Socket client = new Socket(serverName,port);
+            System.out.println("远程主机地址：" + client.getRemoteSocketAddress());
+            OutputStream outToserver = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToserver);
+
+            out.writeUTF("Hello from " + client.getLocalSocketAddress());
+            InputStream inFromServer = client.getInputStream();
+            DataInputStream in = new DataInputStream(inFromServer);
+            System.out.println("服务器相应： " + in.readUTF());
+            client.close();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
