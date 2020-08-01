7dc04866277c29fd32353ff25e1d1ae9f1345e0d
songbing97 <lansahaibing@qq.com>
2020-08-01 16:48:31 +0800

    Before August

---
+++ IO0.java
@@ -0,0 +1,11 @@
+package com.song;
+
+import java.util.Scanner;
+
+public class IO0 {
+    //通过scanner输入
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.nextLine());
+    }
+}
---
+++ IO1.java
@@ -0,0 +1,14 @@
+package com.song;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class IO1 {
+    //通过BufferedReader 、InputStream、 System.in输入
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        System.out.println( br.readLine());
+    }
+}
